425698ed069ddba3da81ecb6b1ddfdb4c07d8dda
Geert Bevin <gbevin@terracottatech.com>
2010-03-29 14:33:42 +0000

    Added Javadocs
    svn path=/projects/terracotta-toolkit-api/branches/expressive/; revision=21354

--- src/main/java/org/terracotta/api/LockType.java
+++ src/main/java/org/terracotta/api/LockType.java
@@ -1,5 +1,29 @@
 package org.terracotta.api;
 
+/**
+ * Enum that contains the  lock types that are supported by Terracotta DSO.
+ *
+ * @author Geert Bevin
+ */
 public enum LockType {
-  READ, WRITE, CONCURRENT, SYNCHRONOUS_WRITE;
+  /**
+   * Asynchronously flush changes to the Terracotta Server Array.
+   */
+  WRITE,
+
+  /**
+   * Synchronously flush changes to the Terracotta Server Array, blocking the calling method until data has been
+   * acknowledged as being committed.
+   */
+  SYNCHRONOUS_WRITE,
+
+  /**
+   * Read locks going up to the Terracotta Server Array.
+   */
+  READ,
+
+  /**
+   * Dirty writes and dirty reads that only gives transaction boundaries and no exclusion. Use at your own risk.
+   */
+  CONCURRENT;
 }
