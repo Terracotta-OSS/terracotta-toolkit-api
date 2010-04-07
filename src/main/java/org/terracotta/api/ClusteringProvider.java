d37772b9fe99d1993d88b583264198dfa0bfde98
Geert Bevin <gbevin@terracottatech.com>
2010-04-07 02:59:22 +0000

    Make tim-ehcache-2.0 work with only dep on terracotta-toolkit
    svn path=/projects/terracotta-toolkit-api/branches/expressive/; revision=21540

--- src/main/java/org/terracotta/api/ClusteringProvider.java
+++ src/main/java/org/terracotta/api/ClusteringProvider.java
@@ -3,6 +3,8 @@ package org.terracotta.api;
 import java.util.concurrent.Callable;
 
 import org.terracotta.cluster.TerracottaCluster;
+import org.terracotta.logging.TerracottaLogger;
+import org.terracotta.properties.TerracottaProperties;
 
 public interface ClusteringProvider {
 
@@ -16,7 +18,33 @@ public interface ClusteringProvider {
 
   void monitorExit(Object obj, LockType type);
 
-  TerracottaCluster getTerracottaCluster();
-
+  TerracottaCluster getCluster();
+  
+  /**
+   * Disable eviction on the provided object in case it implements a Terracotta interface that supports this.
+   * 
+   * @param object the object on which eviction should be disabled
+   * @return {@code true} when eviction could be disabled; or {@code false} otherwise
+   */
+  boolean disableEviction(Object object);
+  
+  /**
+   * Enable eviction on the provided object in case it implements a Terracotta interface that supports this.
+   * 
+   * @param object the object on which eviction should be enabled
+   * @return {@code true} when eviction could be enabled; or {@code false} otherwise
+   */
+  boolean enableEviction(Object object);
+  
+  void waitForAllCurrentTransactionsToComplete();
+  
+  void registerBeforeShutdownHook(Runnable beforeShutdownHook);
+  
+  String getClientID();
+  
   String getUUID();
+  
+  TerracottaLogger getLogger(String name);
+  
+  TerracottaProperties getProperties();
 }
