0a67179faf0f3e4c216933a5dabef1b10cfc9cc3
Geert Bevin <gbevin@terracottatech.com>
2010-04-27 08:20:04 +0000

    Removed dependency on express-utils. Added ability to use ClusteringProvider through StandaloneClusteringProvider directly from the express-runtime jar.
    svn path=/projects/terracotta-toolkit-api/trunk/; revision=22320

--- src/main/java/org/terracotta/express/Client.java
+++ src/main/java/org/terracotta/express/Client.java
@@ -3,6 +3,8 @@ package org.terracotta.express;
 public interface Client {
   <T> T instantiate(String className, Class[] cstrArgTypes, Object[] cstrArgs) throws Exception;
 
+  <T> T staticFactoryMethod(String className, String methodName, Class[] argTypes, Object[] args) throws Exception;
+  
   void shutdown();
 
   void introspectModules(Class source);
