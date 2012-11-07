05b39547bc046b06c067eca3346d4a1a370add90
Nihit Purwar <npurwar@terracottatech.com>
2012-11-07 08:10:03 +0000

    add non stop exception
    svn path=/projects/terracotta-toolkit-api/branches/nonstop/; revision=37496

---
+++ toolkit-api/src/main/java/org/terracotta/toolkit/nonstop/NonStopException.java
@@ -0,0 +1,10 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.toolkit.nonstop;
+
+public class NonStopException extends RuntimeException {
+  public NonStopException(String msg) {
+    super(msg);
+  }
+}
