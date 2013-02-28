07611caa352031d2a7f1d3d7965f333992022052
vitali funshste <vfunshte-unknown@terracottatech.com>
2013-02-28 04:53:16 +0000

    Updated javadoc.
    svn path=/projects/terracotta-toolkit-api/trunk/; revision=38838

--- toolkit-api/src/main/java/org/terracotta/toolkit/search/SearchableMap.java
+++ toolkit-api/src/main/java/org/terracotta/toolkit/search/SearchableMap.java
@@ -8,12 +8,12 @@ import org.terracotta.toolkit.search.attribute.ToolkitAttributeExtractor;
 public interface SearchableMap<K, V> {
 
   /**
-   * Set indexed attribute extractor. This store is considered searchable if this method has been called at least once.
+   * Set indexed attribute extractor. This map is considered searchable if this method has been called at least once.
    */
   void setAttributeExtractor(ToolkitAttributeExtractor<K, V> attrExtractor);
 
   /**
-   * Create a new query builder to construct a search query tied to this store
+   * Create a new query builder to construct a search query tied to this map
    */
   QueryBuilder createQueryBuilder();
 }
