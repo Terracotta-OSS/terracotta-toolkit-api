3c3ec7a3c7cacb6073735254a3a27f534922f133
eshelest <eshelest-unknown@terracottatech.com>
2013-06-13 23:13:52 +0000

    DEV-9829. terracotta-toolkit-api-internal updated, the new version should be released.
    svn path=/projects/terracotta-toolkit-api/trunk/; revision=40346

--- toolkit-api-internal/src/main/java/org/terracotta/toolkit/internal/cache/ToolkitCacheInternal.java
+++ toolkit-api-internal/src/main/java/org/terracotta/toolkit/internal/cache/ToolkitCacheInternal.java
@@ -23,23 +23,21 @@ public interface ToolkitCacheInternal<K, V> extends ToolkitCache<K, V> {
   /**
    * Performs an unlocked put. This method does not take any lock so strong cache behaves like eventual cache. If user
    * takes lock outside then eventual cache will behave like strong cache.
-   * 
-   * @param map input map
    */
   void unlockedPutNoReturn(K k, V v, int createTime, int customTTI, int customTTL);
+
+  void unlockedPutNoReturnVersioned(K k, V v, long version, int createTime, int customTTI, int customTTL);
 
   /**
    * Performs an unlocked remove. This method does not take any lock so strong cache behaves like eventual cache. If
    * user takes lock outside then eventual cache will behave like strong cache.
-   * 
-   * @param map input map
    */
   void unlockedRemoveNoReturn(Object k);
 
+  void unlockedRemoveNoReturnVersioned(Object key, long version);
+
   /**
    * Performs an unlocked get.
-   * 
-   * @param map input map
    */
   V unlockedGet(Object k, boolean quiet);
 
@@ -52,11 +50,11 @@ public interface ToolkitCacheInternal<K, V> extends ToolkitCache<K, V> {
 
   /**
    * Perform a local unlocked read for the given key.
-   * <p>
+   * <p/>
    * This method performs no locking and thus provides absolutely no visibility guarantees. If the value is not local it
    * will NOT be looked up from elsewhere and null is returned instead - thus one cannot distinguish between a
    * non-existent mapping from one where its present elsewhere. USE WITH CAUTION.
-   * 
+   *
    * @param key The key to lookup
    * @return value mapped to {@code key} if present locally in the current node, otherwise null.
    */
@@ -64,7 +62,7 @@ public interface ToolkitCacheInternal<K, V> extends ToolkitCache<K, V> {
 
   /**
    * Query whether key is present locally or not
-   * 
+   *
    * @return true if key is local, otherwise false
    */
   boolean containsLocalKey(Object key);
@@ -72,28 +70,28 @@ public interface ToolkitCacheInternal<K, V> extends ToolkitCache<K, V> {
   /**
    * Return the count of local entries. <br>
    * The count returned might not be very accurate if maxEntriesLocalHeap is set to small values.
-   * 
+   *
    * @return count of local entries
    */
   int localSize();
 
   /**
    * Returns a Set containing keys in the Store which are present in the node locally. The returned set is immutable.
-   * 
+   *
    * @return Set of keys which are present in the node locally.
    */
   Set<K> localKeySet();
 
   /**
    * Return the on-heap size in bytes present locally in current node.<br>
-   * 
+   *
    * @return the on-heap size in bytes present locally in current node.
    */
   long localOnHeapSizeInBytes();
 
   /**
    * Return the off-heap size in bytes present locally in current node.
-   * 
+   *
    * @return the off-heap size in bytes present locally in current node.
    */
   long localOffHeapSizeInBytes();
@@ -101,40 +99,47 @@ public interface ToolkitCacheInternal<K, V> extends ToolkitCache<K, V> {
   /**
    * Return the number of elements present locally on-heap in current node.<br>
    * The count returned might not be very accurate if maxEntriesLocalHeap is set to small values.
-   * 
+   *
    * @return the number of elements present locally on-heap in current node.
    */
   int localOnHeapSize();
 
   /**
    * Return the number of elements present locally in off-heap of current node.
-   * 
+   *
    * @return the number of elements present locally in off-heap of current node.
    */
   int localOffHeapSize();
 
   /**
    * Returns true if the key is present on-heap in current node.
-   * 
+   *
    * @return true if the key is present on-heap in current node otherwise false.
    */
   boolean containsKeyLocalOnHeap(Object key);
 
   /**
    * Returns true if the key is present off-heap in current node.
-   * 
+   *
    * @return true if the key is present off-heap in current node otherwise false.
    */
   boolean containsKeyLocalOffHeap(Object key);
 
   V put(K key, V value, int createTimeInSecs, int customMaxTTISeconds, int customMaxTTLSeconds);
 
+  void putVersioned(K key, V value, long version);
+
+  void putVersioned(K key, V value, long version, int createTimeInSecs, int customMaxTTISeconds, int customMaxTTLSeconds);
+
   /**
    * Dispose the Store from this node. If present elsewhere, the key-value store is still usable. Trying to use the
    * store after disposing locally will throw {@code IllegalStateException}.
    */
-  public void disposeLocally();
+  void disposeLocally();
 
-  public void removeAll(Set<K> keys);
+  void removeAll(Set<K> keys);
 
+  void removeVersioned(Object key, long version);
+
+  void registerVersionUpdateListener(VersionUpdateListener listener);
 }
